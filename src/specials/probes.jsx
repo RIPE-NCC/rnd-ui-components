6d488cbf52ba65f44d57757801a75c1b8ee6e966
Jasper den Hertog <jdenhertog@ripe.net>
2018-07-13 13:18:18 +0200

    better probe circles

--- src/specials/probes.jsx
+++ src/specials/probes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { oimAntracite, oimClouds } from "../themes/colors";
 
 let nextColor = id =>
   ["#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3", "#a6d854", "#ffd92f", "#e5c494"][
@@ -7,10 +8,11 @@ let nextColor = id =>
   ];
 
 export const StyledProbeSetCircle = styled.g`
-  stroke: ${props => nextColor(props.i)};
+  stroke: ${props => (props.colorize && nextColor(props.i)) || oimAntracite};
   stroke-width: 1px;
-  fill: ${props => nextColor(props.i)};
-  fill-opacity: 0.3;
+  fill: ${props => (props.colorize && nextColor(props.i)) || oimClouds};
+  stroke-opacity: ${props => (props.colorize && 1.0) || 0.3};
+  fill-opacity: ${props => (props.colorize && 0.3) || 1.0};
 
   text {
     font-size: 0.8em;
@@ -19,7 +21,7 @@ export const StyledProbeSetCircle = styled.g`
     fill-opacity: 1;
   }
   rect {
-    fill: ${props => nextColor(props.i)};
+    fill: ${props => (props.colorize && nextColor(props.i)) || oimClouds};
     stroke: none;
   }
 `;
@@ -45,15 +47,19 @@ export class ProbeCircle extends React.Component {
       y = this.props.unitStart[1] * this.props.unitSize[1] + this.props.r + 2,
       r = this.props.r,
       margin = r + 4,
-      xMax = this.props.widthInUnits * this.props.unitSize[0] + r + 2,
+      microM = 2,
+      xMax = this.props.widthInUnits * this.props.unitSize[0] + r + microM,
       xMin = 0,
       xEnd = xUnitsEnd * this.props.unitSize[0] - r,
       yEnd =
-        (this.props.unitStart[1] + wrapAround) * this.props.unitSize[1] + r + 2;
+        (this.props.unitStart[1] + wrapAround) * this.props.unitSize[1] +
+        r +
+        microM;
 
     return (
       <StyledProbeSetCircle {...this.props}>
         <path
+          id={`path${this.props.i}`}
           d={`M${margin + x},${y - r} A ${0.5 * r} ${0.5 * r} 0 0 0 ${margin +
             x} ${y + r}`}
         />
@@ -67,8 +73,14 @@ export class ProbeCircle extends React.Component {
             const itsAWrap = wrapAround > 0 && i < wrapAround,
               wasAWrap = wrapAround > 0 && i === wrapAround,
               stillAWrap = wrapAround > 0 && i > 0 && i < wrapAround,
-              x2 = wasAWrap || stillAWrap ? xMin + 4 : x + margin;
+              x2 = wasAWrap || stillAWrap ? xMin + 2 * microM : x + margin;
             return [
+              <rect
+                x={x2}
+                y={y - r + i * this.props.unitSize[1]}
+                width={((itsAWrap && xMax) || xEnd) - x2}
+                height={2 * r}
+              />,
               <line
                 x1={(itsAWrap && xMax) || xEnd}
                 y1={y + r + i * this.props.unitSize[1]}
@@ -80,18 +92,14 @@ export class ProbeCircle extends React.Component {
                 y1={y - r + i * this.props.unitSize[1]}
                 x2={x2}
                 y2={y - r + i * this.props.unitSize[1]}
-              />,
-              <rect
-                x={x2}
-                y={y - r + i * this.props.unitSize[1]}
-                width={((itsAWrap && xMax) || xEnd) - x2}
-                height={2 * r}
               />
             ];
           })}
-        <text x={0.5 * margin + x + 2} y={y + 4}>
-          {this.props.text}
-        </text>
+        {(this.props.lengthInUnits > 1 || this.props.text.length < 3) && (
+          <text x={0.5 * margin + x + 2 * microM} y={y + 2 * microM}>
+            {this.props.text}
+          </text>
+        )}
       </StyledProbeSetCircle>
     );
   }
