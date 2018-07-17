13e08ccb6fddf25785955adf9accf07f006aa3b5
Jasper den Hertog <jdenhertog@ripe.net>
2018-07-17 17:05:27 +0200

    colored sub-aggregation

--- src/specials/probes.jsx
+++ src/specials/probes.jsx
@@ -1,29 +1,35 @@
 import React from "react";
 import styled from "styled-components";
-import { oimAntracite, oimClouds } from "../themes/colors";
+import { oimAntracite, oimClouds, oimSilver } from "../themes/colors";
 
-let nextColor = id =>
-  ["#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3", "#a6d854", "#ffd92f", "#e5c494"][
+let nextColor = id => {
+  if (typeof id === "string") {
+    id = parseInt(`${id.charCodeAt(0)}${id.charCodeAt(1)}`);
+  }
+  return ["#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3", "#a6d854", "#ffd92f", "#e5c494"][
     id % 7
   ];
+}
 
 export const StyledProbeSetCircle = styled.g`
-  stroke: ${props => (props.colorize && nextColor(props.i)) || oimAntracite};
-  stroke-width: 1px;
-  fill: ${props => (props.colorize && nextColor(props.i)) || oimClouds};
-  stroke-opacity: ${props => (props.colorize && 1.0) || 0.3};
-  fill-opacity: ${props => (props.colorize && 0.3) || 1.0};
+  stroke: ${ oimSilver};
+    stroke-width: 1px;
+    stroke-opacity: ${props => (props.colorize && 1.0) || 0.3};
+    fill-opacity: ${props => (props.colorize && 0.3) || 1.0};
 
-  text {
-    font-size: 0.8em;
-    stroke: none;
-    fill: black;
-    fill-opacity: 1;
-  }
-  rect {
-    fill: ${props => (props.colorize && nextColor(props.i)) || oimClouds};
-    stroke: none;
-  }
+    text {
+      font-size: 0.8em;
+      stroke: none;
+      fill: black;
+      fill-opacity: 1;
+    }
+  } `
+
+const ColoredClosedPath = styled.path`
+  fill: ${ props => props.color || oimClouds};
+  stroke: none;
+  /* stroke-width: 1px; */
+}
 `;
 
 export class ProbeCircle extends React.Component {
@@ -33,7 +39,7 @@ export class ProbeCircle extends React.Component {
       this.props.widthInUnits;
     let wrapAround = Math.floor(
       (this.props.unitStart[0] + this.props.lengthInUnits) /
-        this.props.widthInUnits
+      this.props.widthInUnits
     );
 
     if (xUnitsEnd === 0 && wrapAround > 0) {
@@ -42,8 +48,8 @@ export class ProbeCircle extends React.Component {
     }
 
     const x =
-        (this.props.unitStart[0] % this.props.widthInUnits) *
-        this.props.unitSize[0],
+      (this.props.unitStart[0] % this.props.widthInUnits) *
+      this.props.unitSize[0],
       y = this.props.unitStart[1] * this.props.unitSize[1] + this.props.r + 2,
       r = this.props.r,
       margin = r + 4,
@@ -59,13 +65,19 @@ export class ProbeCircle extends React.Component {
     return (
       <StyledProbeSetCircle {...this.props}>
         <path
-          id={`path${this.props.i}`}
-          d={`M${margin + x},${y - r} A ${0.5 * r} ${0.5 * r} 0 0 0 ${margin +
-            x} ${y + r}`}
+          id={`path${this.props.i} `}
+          d={`M${margin + x}, ${y - r} A ${0.5 * r} ${0.5 * r} 0 0 0 ${
+            margin +
+            x
+            } ${y + r} `}
+          style={{ fill: nextColor(this.props.divsInUnits[0][1]) }}
         />
         <path
-          d={`M${xEnd},${yEnd - r} A ${0.5 * r} ${0.5 *
-            r} 0 0 1 ${xEnd} ${yEnd + r}`}
+          d={`M${xEnd}, ${yEnd - r} A ${0.5 * r} ${
+            0.5 *
+            r
+            } 0 0 1 ${xEnd} ${yEnd + r} `}
+          style={{ fill: nextColor(this.props.divsInUnits[this.props.divsInUnits.length - 1][1]) }}
         />
         {Array(wrapAround + 1)
           .fill(true)
@@ -74,13 +86,44 @@ export class ProbeCircle extends React.Component {
               wasAWrap = wrapAround > 0 && i === wrapAround,
               stillAWrap = wrapAround > 0 && i > 0 && i < wrapAround,
               x2 = wasAWrap || stillAWrap ? xMin + 2 * microM : x + margin;
+
+            let divX = x2;
             return [
-              <rect
-                x={x2}
-                y={y - r + i * this.props.unitSize[1]}
-                width={((itsAWrap && xMax) || xEnd) - x2}
-                height={2 * r}
-              />,
+              this.props.divsInUnits.reduce(([divs, divSum], d) => {
+                const newDiv = this.props.unitStart[0] + divSum;
+                if (
+                  // aggregate starts & ends in this row.
+                  (this.props.widthInUnits * i <= newDiv && newDiv < this.props.widthInUnits * (i + 1))) {
+                  divs = [...divs, [d[0], d[1]]];
+                }
+                else if
+                // starts on an row up & ends at a row down this one
+                ((this.props.widthInUnits * i >= newDiv && (newDiv + d[0]) >= this.props.widthInUnits * (i + 1))) {
+                  divs = [...divs, [d[0], d[1]]];
+                }
+                else if
+                // starts on a row up & ends in this row
+                ((this.props.widthInUnits * i >= newDiv && (newDiv + d[0]) < this.props.widthInUnits * (i + 1) && (newDiv + d[0]) > this.props.widthInUnits * i)
+                ) {
+                  divs = [...divs, [((newDiv + d[0]) % this.props.widthInUnits), d[1]]];
+                }
+                return [divs, divSum + d[0]]
+              }, [[], 0])[0].map((d, ii, arr) => {
+                const divXf = divX;
+                let xWidth, xLast;
+
+                xWidth = d[0] * (((this.props.lengthInUnits) * this.props.unitSize[0] - 2 * r - 2 * microM) / this.props.lengthInUnits);
+
+                // Sort of ugly, would like to refactor this.
+                // If this is the last sub-aggregation in a aggegrate then forcibly draw it al the way
+                // to the beginning of the arc path that closes the aggregate component.
+                if (!((arr.length - 1) === ii)) { xLast = xWidth + divX; } else {
+                  xLast = (itsAWrap && xMax) || xEnd;
+                }
+                divX += xWidth;
+
+                return <ColoredClosedPath color={nextColor(d[1])} d={`M ${divXf} ${y - r + i * this.props.unitSize[1]} H ${xLast} V ${y + r + i * this.props.unitSize[1]} H ${divXf} Z`} />
+              }),
               <line
                 x1={(itsAWrap && xMax) || xEnd}
                 y1={y + r + i * this.props.unitSize[1]}
